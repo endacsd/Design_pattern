544270a8496732e5dbc6f6c00b9f258efac3cd9c
endacsd <1770724257@qq.com>
2020-04-01 19:44:58 +0800

    finsh Bridge_Pattern

---
+++ src/Bridge_Pattern/DrawAPI.java
@@ -0,0 +1,5 @@
+package Bridge_Pattern;
+
+public interface DrawAPI  {
+    void drawCircle(int radius,int x,int y);
+}
---
+++ src/Bridge_Pattern/Shape.java
@@ -0,0 +1,9 @@
+package Bridge_Pattern;
+
+public abstract class Shape {
+    protected  DrawAPI drawAPI;
+    protected Shape(DrawAPI drawAPI){
+        this.drawAPI=drawAPI;
+    }
+    public abstract void draw();
+}
